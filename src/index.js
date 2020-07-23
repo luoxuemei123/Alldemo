0c935c0444aac7e9b7a6d0fe0f1c8f8e578bb815
洛雪 <luoxm@scmsafe.com>
2020-07-23 15:28:45 +0800

    1`

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
+/*
+ * @Author: lx 入口js
+ * @Date: 2020-07-23 15:10:13
+ * @LastEditTime: 2020-07-23 15:27:40
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /demoone/src/index.js
+ */ 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
